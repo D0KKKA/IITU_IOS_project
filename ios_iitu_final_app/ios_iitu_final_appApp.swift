d889e8323e3796a9b29c608ee7833a4ce7bc11c6
D0KKKA <danil.kukurekin@bk.ru>
2025-11-25 10:26:20 +0500

    Initial Commit

---
+++ ios_iitu_final_app/ios_iitu_final_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_iitu_final_appApp.swift
+//  ios_iitu_final_app
+//
+//  Created by Кукурекин  Данил on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct ios_iitu_final_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
